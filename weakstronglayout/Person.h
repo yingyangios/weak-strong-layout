88aa492ed4cbb62b0c11a7e174d6ec1bac6dc59c
yingyangios <835616272@qq.com>
2018-12-05 15:57:18 +0800

    first commit

--- weakstronglayout/Person.h
+++ weakstronglayout/Person.h
@@ -12,6 +12,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Person : NSObject
 
+{
+    __strong id name;
+    __weak id a;
+    __unsafe_unretained id c; //不计算在 weak layout
+    int g; //不计算在weak layout
+    __strong id h;
+    // _autoreleasing 不支持
+    //__autoreleasing id j;
+    
+}
+
 @end
 
 NS_ASSUME_NONNULL_END
--- weakstronglayout/ViewController.m
+++ weakstronglayout/ViewController.m
@@ -7,16 +7,55 @@
 //
 
 #import "ViewController.h"
+#import "Person.h"
 
-@interface ViewController ()
+#import <objc/runtime.h>
 
+@interface ViewController()
 @end
 
 @implementation ViewController
 
+- (NSMutableIndexSet *)ivarLayout:(const uint8_t *)strongLayout {
+    NSMutableIndexSet *layout = [NSMutableIndexSet indexSet];
+    NSUInteger index = 0;
+    
+    while (*strongLayout != '\x00') {
+        //非strong
+        uint8_t upper = (*strongLayout & 0xf0) >> 4;
+        
+        //strong
+        uint8_t lower = (*strongLayout & 0x0f);
+        
+        //得到strong引用的初始index
+        index += upper;
+        
+        //将strong的index，并且放在layout中
+        [layout addIndexesInRange:NSMakeRange(index, lower)];
+        
+        //将index指向最后一位strong引用
+        index += lower;
+        
+        //指针+1
+        strongLayout ++;
+    }
+    return layout;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    const uint8_t *weakLayout = class_getWeakIvarLayout(Person.class);
+    NSMutableIndexSet * layout = [self ivarLayout:weakLayout];
+    [layout enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL * _Nonnull stop) {
+        NSLog(@"index: %zd", idx);
+    }];
+    
+    
+    const uint8_t *strongLayout = class_getIvarLayout(Person.class);
+    layout = [self ivarLayout:strongLayout];
+    [layout enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL * _Nonnull stop) {
+        NSLog(@"index: %zd", idx);
+    }];
 }
 
 
