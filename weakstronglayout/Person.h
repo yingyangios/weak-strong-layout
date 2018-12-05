0b15dbd76e786141b5ef962a5e19e64798b87819
yingyangios <835616272@qq.com>
2018-12-05 15:52:27 +0800

    first commit

---
+++ weakstronglayout/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  weakstronglayout
+//
+//  Created by zhaoyan on 2018/12/4.
+//  Copyright © 2018 baidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ weakstronglayout/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weakstronglayout
+//
+//  Created by zhaoyan on 2018/12/4.
+//  Copyright © 2018 baidu. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
